c27b73e41de8f5880d3e9e13718362be7294ae57
Jin Kwan Kim <kkole3897@gmail.com>
2025-04-20 23:48:36 +0900

    feat(useProduct): useProduct hook 구현
    - products 초기 상태
    - updateProduct
    - addProduct

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,27 @@ import { useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const updateProduct = (updatedProduct: Product) => {
+    const newProducts = products.map((product) => {
+      if (product.id === updatedProduct.id) {
+        return updatedProduct;
+      }
+
+      return product;
+    });
+
+    setProducts(newProducts);
+  };
+
+  const addProduct = (newProduct: Product) => {
+    setProducts([...products, newProduct]);
+  };
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct,
+    addProduct,
   };
 };
